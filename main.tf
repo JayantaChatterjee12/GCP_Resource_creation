33e11a2ee7cba1f8852ccdb89cd71288ba610ad9
testrepo <rajibjoy@rediffmail.com>
2025-04-05 20:38:54 +0530

    Adding destroy

--- main.tf
+++ main.tf
@@ -6,12 +6,13 @@ provider "google" {
 //  source = "./modules/gcp_storage_website"
 //}
 
-//module "gcp_autoscalling" {
-//  source = "./modules/gcp_autoscalling_module"}
-
-module "gcp_vm" {
-  source = "./modules/gcp_vm_module"
+module "gcp_autoscalling" {
+  source = "./modules/gcp_autoscalling_module"
 }
+
+//module "gcp_vm" {
+//  source = "./modules/gcp_vm_module"
+//}
 
 //module "gcp_vpc" {
 //  source = "./modules/gcp_vpc_module"
