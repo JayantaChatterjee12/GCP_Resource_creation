0a515de3a7349f1420ebc6c549e00c02a25a7723
testrepo <rajibjoy@rediffmail.com>
2025-04-09 14:46:55 +0530

    Adding remote state file

--- main.tf
+++ main.tf
@@ -2,9 +2,13 @@ provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
 }
-//module "gcp_website" {
-//  source = "./modules/gcp_storage_website"
-//}
+
+terraform {
+  backend "gcs" {
+    bucket = "tf_lock_file_storing_bucket_for_gcp"
+    prefix = "terraform/state"
+  }
+}
 
 module "gcp_autoscalling" {
   source = "./modules/gcp_autoscalling_module"
@@ -16,4 +20,8 @@ module "gcp_autoscalling" {
 
 //module "gcp_vpc" {
 //  source = "./modules/gcp_vpc_module"
+//}
+
+//module "gcp_website" {
+//  source = "./modules/gcp_storage_website"
 //}
